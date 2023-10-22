09a14b590719f6866469bbfe3c73b3984e878110
SevenImm <angelcardenasptx@gmail.com>
2023-10-21 21:05:48 -0500

    Added saving feature

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-// Get time and display it as text
-let currentTime = setInterval(CurrentToday, 1000);
-function CurrentToday(){
-var today = dayjs();
-$('#currentDay').text(today.format('dddd, MMMM D, YYYY h:mm:ss A'));
-};
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -15,16 +9,32 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $(".saveBtn").on("click", function() {
+    // Get the parent time-block element by traversing the DOM
+    const timeBlock = $(this).closest(".time-block");
+    // Extract the ID from the time-block element
+    const blockId = timeBlock.attr("id");
+    // Get user input from the textarea within the time-block
+    const userDescription = timeBlock.find(".description").val();
+
+    localStorage.setItem(blockId, userDescription);
+  });
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  // Get time and display it as text
+  let currentTime = setInterval(CurrentToday, 1000);
+  function CurrentToday() {
+    var today = dayjs();
+    $('#currentDay').text(today.format('dddd, MMMM D, YYYY h:mm:ss A'));
+  };
+
 });
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Feature/TimeLayout:
 For this branch I want to finish the sheduler from 9AM to 5PM.
 
+## Feature/SaveBtn:
+For this branch I want to save the text user input for later display:
+- Add an event to a hour,
+- Click the save button,
+- The text should be saved. Separated by hour(id)
